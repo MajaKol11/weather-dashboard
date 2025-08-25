d45fd5022bc200f3e52506fadca600a6707c0309
MajaKol11 <majexwork@yahoo.com>
2025-08-25 19:56:41 +0100

    Feature: Geocoding with errors and UI feedback.

--- src/app.js
+++ src/app.js
@@ -19,5 +19,31 @@ form.addEventListener("submit", async (e) => {
         return; 
     }
 
-    setStatus("Searching...");
+    (async () => {
+        try {
+            setStatus("Searching...");
+            const place = await fetchCoords(q);
+            setStatus(`Found ${place.name}${place.state ? "," + place.state : ""} ${place.country}. Fetching weather...`)
+        }
+        catch (err) {
+            setStatus(err.message || "Failed to search location.");
+        }
+    })();
+
+
+    async function fetchCoords(q) {
+        const url = `https://api.openweathermap.org/geo/1.0/direct?q=${encodeURIComponent(q)}&limit=1&appid=${OWM_API_KEY}`;
+        const resp = await fetch(url, { method: "GET" });
+
+        if (!resp.ok) throw new Error(`Geocoding failed: ${resp.statusText}`);
+        const data = await resp.json();
+
+        if (!Array.isArray(data) || data.length ===0) 
+        {
+            throw new Error("City not found. Try City or City,CountryCode (e.g. London,UK)");
+        }
+
+        const {lat, lon, name, country, state} = data[0];
+        return {lat, lon, name, country, state};
+    }
 });
